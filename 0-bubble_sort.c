f6ef284abc598958a0c1012e5444df1d29822e2e
Muthoni Njoki <fmuthoninjoki@gmail.com>
2023-11-20 16:24:30 +0300

    Function that sorts a doubly linked list of integers in asceding order

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,7 +6,8 @@
  * @array: The array to be sorted
  * @size: Number of elements in the array 
  * 
- */void bubble_sort(int *array, size_t size)
+ */
+void bubble_sort(int *array, size_t size)
 {
     size_t i, j, n;
 
